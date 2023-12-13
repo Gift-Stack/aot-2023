febecf0ab9c9776031af0659833c4f7306bff9e7
Gift Opia <gkenny896@gmail.com>
2023-12-13 14:04:35 +0100

    Create 11.ts

---
+++ 11.ts
@@ -0,0 +1,54 @@
+type SantaListProtector<T> = Readonly<{
+	[K in keyof T]: T[K] extends Record<string, unknown> | unknown[] ? SantaListProtector<T[K]> : T[K]
+}>;
+
+
+type SomeTest = SantaListProtector<{
+  //   ^?
+  hacksore: () => 'naughty';
+  trash: string;
+  elliot: {
+    penny: boolean;
+    candace: {
+      address: {
+        street: {
+          name: 'candy cane way';
+          num: number;
+        };
+        k: 'hello';
+      };
+      children: [
+        'harry',
+        {
+          saying: ['hey'];
+        },
+      ];
+    };
+  };
+}>;
+
+// Returns:
+/**
+ * type test_0_expected = {
+  readonly hacksore: () => 'naughty';
+  readonly trash: string;
+  readonly elliot: {
+    readonly penny: boolean;
+    readonly candace: {
+      readonly address: {
+        readonly street: {
+          readonly name: 'candy cane way';
+          readonly num: number;
+        };
+        readonly k: 'hello';
+      };
+      readonly children: readonly [
+        'harry',
+        {
+          readonly saying: readonly ['hey'];
+        },
+      ];
+    };
+  };
+};
+ */
